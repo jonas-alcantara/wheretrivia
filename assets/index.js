90c728e35682585e9b014321bc626ff471f0f129
Jonas Alcantara <alcantarajonassibal@gmail.com>
2021-04-28 02:51:32 +0800

    done with fb and twitter sharing but not pinterest

--- assets/index.js
+++ assets/index.js
@@ -152,7 +152,23 @@ var Slider = (function () {
     return Slider;
 })();
 var slide;
+
 document.addEventListener('DOMContentLoaded', function () {
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '2084227675196220',
+            autoLogAppEvents: true,
+            xfbml: true,
+            version: 'v10.0'
+        });
+    };
+    document.getElementById('fb_share_btn').onclick = function () {
+        FB.ui({
+            display: 'popup',
+            method: 'share',
+            href: 'https://jonas-alcantara.github.io/wheretrivia/',
+        }, function (response) { });
+    }
     slide = new Slider({
         autoPlay: true,
         autoPlayInterval: 5000
